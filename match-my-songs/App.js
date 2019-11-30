d4f8f1c1dc378c6394d0653262c8d7344f9291d4
Jiezhuo <jiezhuo.lyu@gmail.com>
2019-11-29 23:48:45 -0600

    create bottom tab navigation

--- match-my-songs/App.js
+++ match-my-songs/App.js
@@ -1,18 +1,38 @@
 import * as React from 'react';
 import { NavigationNativeContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import PlaylistScreen from './src/playlist.page';
 import LoginScreen from './src/loginPage';
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return(
     <NavigationNativeContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Playlist" component={PlaylistScreen}/>
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Login') {
+              iconName = `ios-information-circle${focused? '' : '-outline'}`
+              return <Ionicons name={iconName} color={color} size={size}/>
+            } else if (route.name === 'Playlist'){
+              iconName = `music-circle${focused? '' : '-outline'}`
+              return <MaterialCommunityIcons name={iconName} color={color} size={size}/>
+            }
+
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Login" component={LoginScreen}/>
+        <Tab.Screen name="Playlist" component={PlaylistScreen}/>
+      </Tab.Navigator>
     </NavigationNativeContainer>
   )
 }
